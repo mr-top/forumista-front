96dbbc9c392dfd6ce60a9ed395c212d18a10994e
Admin <tserenlkhagvaganbold@gmail.com>
2024-11-28 22:47:08 +0000

    board and not found page

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,27 @@
 function Board(props) {
-  const { option = 'medium', children } = props;
+  const { option, children } = props;
 
-  let classes;
+  let classesX;
+  let classesY;
 
   switch (option) {
     case 'small':
-
+      classesX = 'w-4/6 sm:w-2/6';
+      classesY = 'min-h-[10rem]';
+      break;
+    case 'large':
+      classesX = 'w-5/6 sm:w-5/6';
+      classesY = 'min-h-[10rem]';
+      break;
+    default:
+      classesX = 'w-5/6 sm:w-4/6';
+      classesY = 'min-h-[10rem]';
   }
-
 
   return (
     <>
-    <div className="flex flex-row flex-auto items-center w-5/6">
-      <div className="flex-auto bg-base-100 h-3/4">
+    <div className={`flex flex-row flex-auto items-center my-10 ${classesX}`}>
+      <div className={`flex-auto bg-base-100 rounded-md ${classesY}`}>
         {children}
       </div>
     </div>
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,19 @@ import Board from "./Board";
 function NotFound () {
   return (
     <Board option='small'>
-      <p>Hello World</p>
+      <div className="flex flex-row items-center h-full m-5">
+        <div className="flex flex-col flex-auto items-center">
+          <div className="flex-auto max-w-[20rem] space-y-3">
+            <p className="text-2xl text-center">404 Not Found</p>
+            <p>The page that you are looking for is not found. Please navigate to other accessible pages below</p>
+            <div className="flex flex-col">
+              <a className="link flex-none">Home</a>
+              <a className="link flex-none">Help And Support</a>
+            </div>
+            <p>If you believe this was an error. Contact us at <a className="link">generic@help.com</a></p>
+          </div>
+        </div>
+      </div>
     </Board>
   )
 }
